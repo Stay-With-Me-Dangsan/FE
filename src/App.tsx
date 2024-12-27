c98efd31b0a92f2f1f1ad633425a0a904c8076ca
jm4293 <dlwoals4293@gmail.com>
2024-12-27 11:02:06 +0900

    [Feat]: portal을 이용한 modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import './App.css';
 import { Router } from './router/Router';
+import { Modal } from './components/modal';
 
 function App() {
-  return <Router />;
+  return (
+    <>
+      <Router />
+      <Modal>
+        <h2>모달</h2>
+        <p>모달 내용</p>
+      </Modal>
+    </>
+  );
 }
 
 export default App;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,3 +1,15 @@
+import { useSetAtom } from 'jotai';
+import { modalStore } from '../../store';
+
 export const Home = () => {
-    return <div className="h-[1520px] bg-amber-50">홈</div>;
+  const openModal = useSetAtom(modalStore.openModal); // 상태 변경 함수 사용
+
+  return (
+    <>
+      <div className="h-[1520px] bg-amber-50">
+        <p>홈</p>
+        <button onClick={openModal}>Open Modal</button>
+      </div>
+    </>
+  );
 };
