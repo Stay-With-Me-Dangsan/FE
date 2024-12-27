8fa5723def139d5c56889bf20144a66da3716960
jm4293 <dlwoals4293@gmail.com>
2024-12-27 10:12:46 +0900

    [Design]: Navbar

--- src/common/layout/Navbar.tsx
+++ src/common/layout/Navbar.tsx
@@ -1,19 +1,27 @@
-import {useNavigate} from "react-router-dom";
+import { useLocation, useNavigate } from 'react-router-dom';
+import { NavbarBoard, NavbarHome, NavbarMap, NavbarMyPage } from '../../asset/svg/navbar';
+import { useState } from 'react';
 
 export const Navbar = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  const location = useLocation();
 
-    const onClickHandler = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-        e.stopPropagation();
+  const [pathname, setPathname] = useState<string>(location.pathname);
 
-        const { id } = e.currentTarget
-        navigate(`/${id}`)
-    }
+  const onClickHandler = (event: React.MouseEvent<SVGSVGElement, MouseEvent>) => {
+    event.stopPropagation();
 
-    return <div className="absolute bottom-0 w-full h-[80px] z-50 flex justify-around items-center border-t-[1px] border-black bg-white">
-        <div className="cursor-pointer" onClick={e => onClickHandler(e)} id="home">홈</div>
-        <div className="cursor-pointer" onClick={e => onClickHandler(e)} id="map">지도</div>
-        <div className="cursor-pointer" onClick={e => onClickHandler(e)} id="board">게시판</div>
-        <div className="cursor-pointer" onClick={e => onClickHandler(e)} id="mypage">마이페이지</div>
+    const { id } = event.currentTarget;
+    navigate(id);
+    setPathname(id);
+  };
+
+  return (
+    <div className="absolute bottom-0 w-full h-[108px] z-50 flex justify-around items-center border-t-[1px] border-black bg-white">
+      <NavbarHome color={pathname === 'home' ? '#9470DC' : '#989898'} onClick={onClickHandler} />
+      <NavbarMap color={pathname === 'map' ? '#9470DC' : '#989898'} onClick={onClickHandler} />
+      <NavbarBoard color={pathname === 'board' ? '#9470DC' : '#989898'} onClick={onClickHandler} />
+      <NavbarMyPage color={pathname === 'mypage' ? '#9470DC' : '#989898'} onClick={onClickHandler} />
     </div>
-}
+  );
+};
