f7fcde7f4c83c28217d819beaa8df579ebae42f8
jm4293 <dlwoals4293@gmail.com>
2024-12-27 10:21:07 +0900

    [Fix]: Navbar

--- src/common/layout/Layout.tsx
+++ src/common/layout/Layout.tsx
@@ -1,13 +1,13 @@
-import {Outlet} from "react-router-dom";
-import {Navbar} from "./Navbar";
+import { Outlet } from 'react-router-dom';
+import Navbar from './Navbar';
 
 export const Layout = () => {
-    return (
-        <div className="relative w-[960px] min-h-[100vh] max-h-[100vh] mx-auto flex flex-col overflow-hidden">
-            <div className="overflow-auto z-40 mb-[80px] w-full">
-                <Outlet/>
-            </div>
-            <Navbar />
-        </div>
-    );
+  return (
+    <div className="relative w-[960px] min-h-[100vh] max-h-[100vh] mx-auto flex flex-col overflow-hidden">
+      <div className="overflow-auto z-40 mb-[80px] w-full">
+        <Outlet />
+      </div>
+      <Navbar />
+    </div>
+  );
 };
--- src/common/layout/Navbar.tsx
+++ src/common/layout/Navbar.tsx
@@ -1,27 +1,29 @@
 import { useLocation, useNavigate } from 'react-router-dom';
 import { NavbarBoard, NavbarHome, NavbarMap, NavbarMyPage } from '../../asset/svg/navbar';
-import { useState } from 'react';
+import { memo, useCallback, useState } from 'react';
 
-export const Navbar = () => {
+const Navbar = () => {
   const navigate = useNavigate();
   const location = useLocation();
 
   const [pathname, setPathname] = useState<string>(location.pathname);
 
-  const onClickHandler = (event: React.MouseEvent<SVGSVGElement, MouseEvent>) => {
+  const onClickHandler = useCallback((event: React.MouseEvent<SVGSVGElement, MouseEvent>) => {
     event.stopPropagation();
 
     const { id } = event.currentTarget;
+    setPathname(`/${id}`);
     navigate(id);
-    setPathname(id);
-  };
+  }, []);
 
   return (
     <div className="absolute bottom-0 w-full h-[108px] z-50 flex justify-around items-center border-t-[1px] border-black bg-white">
-      <NavbarHome color={pathname === 'home' ? '#9470DC' : '#989898'} onClick={onClickHandler} />
-      <NavbarMap color={pathname === 'map' ? '#9470DC' : '#989898'} onClick={onClickHandler} />
-      <NavbarBoard color={pathname === 'board' ? '#9470DC' : '#989898'} onClick={onClickHandler} />
-      <NavbarMyPage color={pathname === 'mypage' ? '#9470DC' : '#989898'} onClick={onClickHandler} />
+      <NavbarHome color={pathname === '/home' ? '#9470DC' : '#989898'} onClick={onClickHandler} />
+      <NavbarMap color={pathname === '/map' ? '#9470DC' : '#989898'} onClick={onClickHandler} />
+      <NavbarBoard color={pathname === '/board' ? '#9470DC' : '#989898'} onClick={onClickHandler} />
+      <NavbarMyPage color={pathname === '/mypage' ? '#9470DC' : '#989898'} onClick={onClickHandler} />
     </div>
   );
 };
+
+export default memo(Navbar);
