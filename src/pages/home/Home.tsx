10ee949631f9e8eea1cb291b28d64b8fc1da8c91
jm4293 <dlwoals4293@gmail.com>
2025-01-07 23:00:49 +0900

    [Chore]: CORS 테스트 위한 코드 푸시

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,6 @@
 import { useSetAtom } from 'jotai';
 import { modalStore } from '../../store';
+import axios from 'axios';
 
 export const Home = () => {
   const openModal = useSetAtom(modalStore.openModal); // 상태 변경 함수 사용
@@ -9,6 +10,16 @@ export const Home = () => {
       <div className="h-[1520px] bg-amber-50">
         <p>홈</p>
         <button onClick={openModal}>Open Modal</button>
+
+        <button
+          onClick={() => {
+            axios.post('http://localhost:8080/house/getDetail', {
+              houseDetailId: 7,
+            });
+          }}
+          style={{ border: '1px solid black' }}>
+          CORS 오류 확인 버튼
+        </button>
       </div>
     </>
   );
