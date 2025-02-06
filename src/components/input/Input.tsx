5f62d05c084a874d386675fe5e927e1efdbaa0c4
jm4293 <dlwoals4293@gmail.com>
2025-02-06 22:42:00 +0900

    [Refactor]: input 컴포넌트 border-color Props 추가

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -3,12 +3,25 @@ interface IProps {
   value: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   placeholder?: string;
+  borderColor?: 'gray' | 'purple';
 }
 
-export const Input = ({ type, value, onChange, placeholder }: IProps) => {
+export const Input = ({ type, value, onChange, placeholder, borderColor }: IProps) => {
+  const border_color = () => {
+    switch (borderColor) {
+      case 'gray':
+        return '#CDCDCD';
+      case 'purple':
+        return '#9470DC';
+      default:
+        return '#CDCDCD';
+    }
+  };
+
   return (
     <input
-      className="w-full pt-[24px] pb-[24px] pr-[20px] pl-[20px] font-normal text-2xl border-[1px] border-input-border rounded-xl"
+      className="w-full py-[24px] px-[20px] font-normal text-2xl border-[1px] rounded-xl"
+      style={{ borderColor: border_color() }}
       type={type}
       value={value}
       onChange={onChange}
