ad9a84eaa1fd7cee2bfa72f49f3f0c10f4934f3d
jm4293 <dlwoals4293@gmail.com>
2025-02-18 01:41:57 +0900

    [Init]: KAKAO MAP

--- src/pages/map/Map.tsx
+++ src/pages/map/Map.tsx
@@ -1,10 +1,21 @@
 import { Maps, Search } from './components';
+import { useEffect, useState } from 'react';
 
 export const Map = () => {
+  const [latitude, setLatitude] = useState<number | null>(null); //위도
+  const [longitude, setLongitude] = useState<number | null>(null); //경도
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setLatitude(position.coords.latitude);
+      setLongitude(position.coords.longitude);
+    });
+  }, []);
+
   return (
     <div className="h-[9000px] bg-green-50">
       <Search />
-      <Maps />
+      <Maps {...{ latitude, setLatitude, longitude, setLongitude }} />
     </div>
   );
 };
--- src/pages/map/components/Maps.tsx
+++ src/pages/map/components/Maps.tsx
@@ -1,3 +1,28 @@
-export const Maps = () => {
-  return <></>;
+import { Map, useKakaoLoader } from 'react-kakao-maps-sdk';
+
+interface IProps {
+  latitude: number | null;
+  setLatitude: React.Dispatch<React.SetStateAction<number | null>>;
+  longitude: number | null;
+  setLongitude: React.Dispatch<React.SetStateAction<number | null>>;
+}
+
+export const Maps = (props: IProps) => {
+  const { longitude, setLongitude, latitude, setLatitude } = props;
+
+  const [loading] = useKakaoLoader({ appkey: process.env.REACT_APP_KAKAO_APP_KEY as string });
+
+  if (loading) {
+    return <div>loading...</div>;
+  }
+
+  if (!latitude || !longitude) {
+    return <div>위치 정보가 없습니다.</div>;
+  }
+
+  return (
+    <div className="h-full">
+      <Map className="w-full h-full" center={{ lat: latitude, lng: longitude }}></Map>;
+    </div>
+  );
 };
