bc0fd09d3dfaf14dfabaee474a81e800bcd17234
jm4293 <dlwoals4293@gmail.com>
2025-02-18 23:43:33 +0900

    [Fix]: 브라우저 위치정보 권한 해제

--- src/pages/map/Map.tsx
+++ src/pages/map/Map.tsx
@@ -1,16 +1,9 @@
 import { Maps, Search } from './components';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 export const Map = () => {
-  const [latitude, setLatitude] = useState<number | null>(37.553836); //위도
-  const [longitude, setLongitude] = useState<number | null>(126.969652); //경도
-
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition((position) => {
-      setLatitude(position.coords.latitude);
-      setLongitude(position.coords.longitude);
-    });
-  }, []);
+  const [latitude, setLatitude] = useState<number>(37.55586671127581); //위도
+  const [longitude, setLongitude] = useState<number>(126.97207997617642); //경도
 
   return (
     <div className="h-[9000px] bg-green-50">
--- src/pages/map/components/Maps.tsx
+++ src/pages/map/components/Maps.tsx
@@ -1,28 +1,51 @@
-import { Map, useKakaoLoader } from 'react-kakao-maps-sdk';
+import { Map, useKakaoLoader, MapMarker } from 'react-kakao-maps-sdk';
+import { useCallback, useState } from 'react';
 
 interface IProps {
-  latitude: number | null;
-  setLatitude: React.Dispatch<React.SetStateAction<number | null>>;
-  longitude: number | null;
-  setLongitude: React.Dispatch<React.SetStateAction<number | null>>;
+  latitude: number;
+  setLatitude: React.Dispatch<React.SetStateAction<number>>;
+  longitude: number;
+  setLongitude: React.Dispatch<React.SetStateAction<number>>;
 }
 
 export const Maps = (props: IProps) => {
   const { longitude, setLongitude, latitude, setLatitude } = props;
 
+  const [zoomLevel, setZoomLevel] = useState<number>(3);
+
   const [loading] = useKakaoLoader({ appkey: process.env.REACT_APP_KAKAO_APP_KEY as string });
+
+  const onMapClickHandler = (target: kakao.maps.Map, mouseEvent: kakao.maps.event.MouseEvent) => {
+    console.log('getCenter', target.getCenter());
+
+    console.log('mouseEvent', mouseEvent);
+  };
+
+  const onMapZoomChangedHandler = (target: kakao.maps.Map) => {
+    setZoomLevel(target.getLevel());
+  };
+
+  const onMarkerClickHandler = (marker: kakao.maps.Marker) => {
+    console.log('marker', marker.getPosition());
+  };
 
   if (loading) {
     return <div>loading...</div>;
   }
 
-  if (!latitude || !longitude) {
-    return <div>위치 정보가 없습니다.</div>;
-  }
-
   return (
-    <div className="h-full">
-      <Map className="w-full h-full" center={{ lat: latitude, lng: longitude }}></Map>;
-    </div>
+    <Map
+      className="w-full h-full"
+      center={{ lat: latitude, lng: longitude }}
+      level={zoomLevel}
+      onClick={(target, mouseEvent) => onMapClickHandler(target, mouseEvent)}
+      onZoomChanged={(target) => onMapZoomChangedHandler(target)}>
+      <MapMarker
+        position={{ lat: latitude, lng: longitude }}
+        title="111111111"
+        draggable
+        onClick={(marker) => onMarkerClickHandler(marker)}
+      />
+    </Map>
   );
 };
