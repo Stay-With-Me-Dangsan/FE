b5c637843b48207aa2837029eaa2269bbd455d09
jm4293 <dlwoals4293@gmail.com>
2025-02-26 00:40:30 +0900

    [Design]: Header

--- src/common/layout/Layout.tsx
+++ src/common/layout/Layout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from './Navbar';
+import { Header } from './Header';
 
 export const Layout = () => {
   return (
     <div className="layout">
+      <Header />
       <div className="outlet">
         <Outlet />
       </div>
